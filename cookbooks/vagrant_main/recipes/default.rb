2929260b545911e1d7c6cc045122bb4c9fbaa122
Tomaz Muraus <tomaz@tomaz.me>
2011-03-29 15:17:24 +0200

    Create Cast root directory on provisioning.

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -14,3 +14,15 @@ include_recipe "npm"
         action :install
     end
 end
+
+# Cast specific stuff
+bash "create_cast_directories" do
+  user "root"
+
+  cwd "/tmp/"
+  code <<-EOH
+    mkdir -p /opt/cast/services-enabled
+    chown -R vagrant:vagrant /opt/cast
+  EOH
+  not_if { FileTest.exists?("/opt/cast/services-enabled") }
+end
